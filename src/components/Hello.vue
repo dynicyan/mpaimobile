aa6b5a276f5fc4832fededdf0df9d644e6f92c3b
dynicyan <dynicyan@qq.com>
2017-08-21 15:49:34 +0800

    mobile

---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="type-b">
+    <h1 class="h1">{{ msg }}</h1>
+    <h2>你好，我是移动新朋友，喵～</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+  // @import "../assets/scss/reset"
+  @import "../assets/scss/px2rem.scss"
+  .h1
+    font-size: px2rem(36)
+  .type-b
+    width: px2rem(500)
+    height: px2rem(500)
+    background: #ffd600
+    margin: 0 auto
+    h2
+      font-size: px2rem(24)
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App'
+import router from './router'
+Vue.use(VueResource)
+Vue.config.productionTip = false
+import './assets/base.css'
+import './assets/flexMobile.js'
+/* eslint-disable no-new */
+new Vue({
+  router,
+  data: {
+    Bus: new Vue()
+  },
+  render: h => h(App)
+}).$mount('#app')
